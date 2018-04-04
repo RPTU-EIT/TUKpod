751f1978fe749d0429e46417ddd86766c7c4c060
Janis Krieger <37302732+janiskrieger@users.noreply.github.com>
2018-04-04 11:33:53 +0200

    Update Versuch_2.ino

--- versuche/Versuch_2/Versuch_2.ino
+++ versuche/Versuch_2/Versuch_2.ino
@@ -1,4 +1,6 @@
 //Versuch 2
+//Caution: set Tools/USB Type/Audio !
+
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -6,23 +8,36 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S lineIn; //xy=830
-AudioOutputI2S audioOut; //xy=980
-AudioConnection patchCord1(lineIn, 0, audioOut, 0);
-AudioConnection patchCord2(lineIn, 1, audioOut, 1);
-AudioControlSGTL5000 audioShield; //xy=830,40
+AudioInputUSB            usbIn;           //xy=488,247
+AudioOutputI2S           audioOut;       //xy=648,247
+AudioConnection          patchCord1(usbIn, 0, audioOut, 0);
+AudioConnection          patchCord2(usbIn, 1, audioOut, 1);
+AudioControlSGTL5000     audioShield;    //xy=501,286
 // GUItool: end automatically generated code
 
-const int inputSource = AUDIO_INPUT_LINEIN; //AUDIO_INPUT_MIC;
+
+int volumePin = A12;    //analog pin 12
+float maxVolume = 0.7;  //I do not recommend 0.8 .
 
 void setup() {
-	AudioMemory(12);
-
-	audioShield.enable();
-	audioShield.inputSelect(inputSource);
-	audioShield.volume(0.5);
+  AudioMemory(12);
+  audioShield.enable();
+  audioShield.volume(0);
+  pinMode(volumePin, INPUT);
 }
 
+float vol;
+
 void loop() {
-	delay(100);
+  //read analog value from poti
+  vol = analogRead(volumePin);    //The PC volume does not change the teensy volume.
+                                  //Use vol = usbIn.volume(); instead of vol = analogRead(volumePin);
+  //sets the volume to a range between 0 and 1023
+  vol = constrain(vol, 0, 1023);
+  //transform it to a range between 0 and maxVolume
+  vol = (vol/1023)*maxVolume;
+
+  //set volume
+  audioShield.volume(vol);
+  delay(20);
 }
