1d7cbb7d02a43e65224cf1188911847c56ba254c
Janis Krieger <37302732+janiskrieger@users.noreply.github.com>
2018-04-22 15:50:28 +0200

    fixed routing error
    fixed routing error

--- versuche/Versuch_4/Versuch_4.ino
+++ versuche/Versuch_4/Versuch_4.ino
@@ -6,32 +6,35 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S lineIn; //xy=349,269
-AudioFilterBiquad biquadLowpassRight; //xy=587,189
-AudioFilterBiquad biquadLowpassLeft; //xy=589,153
-AudioFilterBiquad biquadHighpassLeft; //xy=589,249
-AudioFilterBiquad biquadHighpassRight; //xy=589,288
-AudioFilterBiquad biquadBandpassLeft; //xy=591,338
-AudioFilterBiquad biquadBandpassRight; //xy=591,377
-AudioMixer4 mixerRight; //xy=824,311
-AudioMixer4 mixerLeft; //xy=825,221
-AudioOutputI2S lineOut; //xy=978,265
-AudioConnection patchCord1(lineIn, 0, biquadLowpassLeft, 0);
-AudioConnection patchCord2(lineIn, 0, biquadHighpassLeft, 0);
-AudioConnection patchCord3(lineIn, 0, biquadBandpassLeft, 0);
-AudioConnection patchCord4(lineIn, 1, biquadHighpassRight, 0);
-AudioConnection patchCord5(lineIn, 1, biquadLowpassRight, 0);
-AudioConnection patchCord6(lineIn, 1, biquadHighpassRight, 0);
-AudioConnection patchCord7(biquadLowpassRight, 0, mixerRight, 0);
-AudioConnection patchCord8(biquadLowpassLeft, 0, mixerLeft, 0);
-AudioConnection patchCord9(biquadHighpassLeft, 0, mixerLeft, 1);
-AudioConnection patchCord10(biquadHighpassRight, 0, mixerRight, 1);
-AudioConnection patchCord11(biquadBandpassLeft, 0, mixerLeft, 2);
-AudioConnection patchCord12(biquadHighpassRight, 0, mixerRight, 2);
-AudioConnection patchCord13(mixerRight, 0, lineOut, 1);
-AudioConnection patchCord14(mixerLeft, 0, lineOut, 0);
-AudioControlSGTL5000 audioShield; //xy=364,383
+AudioInputI2S            lineIn;         //xy=525,332
+AudioFilterBiquad        biquadLowpassRight; //xy=763,252
+AudioFilterBiquad        biquadLowpassLeft; //xy=765,216
+AudioFilterBiquad        biquadHighpassLeft; //xy=765,312
+AudioFilterBiquad        biquadHighpassRight; //xy=765,351
+AudioFilterBiquad        biquadBandpassLeft; //xy=767,401
+AudioFilterBiquad        biquadBandpassRight; //xy=767,440
+AudioMixer4              mixerRight;     //xy=1000,374
+AudioMixer4              mixerLeft;      //xy=1001,284
+AudioOutputI2S           lineOut;        //xy=1154,328
+AudioConnection          patchCord1(lineIn, 0, biquadLowpassLeft, 0);
+AudioConnection          patchCord2(lineIn, 0, biquadHighpassLeft, 0);
+AudioConnection          patchCord3(lineIn, 0, biquadBandpassLeft, 0);
+AudioConnection          patchCord4(lineIn, 1, biquadHighpassRight, 0);
+AudioConnection          patchCord5(lineIn, 1, biquadLowpassRight, 0);
+AudioConnection          patchCord6(lineIn, 1, biquadHighpassRight, 0);
+AudioConnection          patchCord7(lineIn, 1, biquadBandpassRight, 0);
+AudioConnection          patchCord8(biquadLowpassRight, 0, mixerRight, 0);
+AudioConnection          patchCord9(biquadLowpassLeft, 0, mixerLeft, 0);
+AudioConnection          patchCord10(biquadHighpassLeft, 0, mixerLeft, 1);
+AudioConnection          patchCord11(biquadHighpassRight, 0, mixerRight, 1);
+AudioConnection          patchCord12(biquadBandpassLeft, 0, mixerLeft, 2);
+AudioConnection          patchCord13(biquadBandpassRight, 0, mixerRight, 2);
+AudioConnection          patchCord14(mixerRight, 0, lineOut, 1);
+AudioConnection          patchCord15(mixerLeft, 0, lineOut, 0);
+AudioControlSGTL5000     audioShield;    //xy=540,446
 // GUItool: end automatically generated code
+
+
 
 const int myInput = AUDIO_INPUT_LINEIN; // AUDIO_INPUT_MIC;
 
