f659a10569663ea0c32a6f25fa2e8d39decffddc
Janis Krieger <37302732+janiskrieger@users.noreply.github.com>
2019-05-20 09:47:29 +0200

    Update tukpod.ino
    tukpod.ino mit Animation und integrierter Frequenzweiche

--- src/tukpod.ino
+++ src/tukpod.ino
@@ -1,72 +1,112 @@
-//tukpod - GNU General Public License v3.0
-//https://github.com/TUK-EIT/tukpod
+#include <FastLED.h>
 #include <Audio.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S            lineIn;         //xy=525,332
-AudioFilterBiquad        biquadLowpassRight; //xy=763,252
-AudioFilterBiquad        biquadLowpassLeft; //xy=765,216
-AudioFilterBiquad        biquadHighpassLeft; //xy=765,312
-AudioFilterBiquad        biquadHighpassRight; //xy=765,351
-AudioFilterBiquad        biquadBandpassLeft; //xy=767,401
-AudioFilterBiquad        biquadBandpassRight; //xy=767,440
-AudioMixer4              mixerRight;     //xy=1000,374
-AudioMixer4              mixerLeft;      //xy=1001,284
-AudioOutputI2S           lineOut;        //xy=1154,328
-AudioConnection          patchCord1(lineIn, 0, biquadLowpassLeft, 0);
-AudioConnection          patchCord2(lineIn, 0, biquadHighpassLeft, 0);
-AudioConnection          patchCord3(lineIn, 0, biquadBandpassLeft, 0);
-AudioConnection          patchCord4(lineIn, 1, biquadHighpassRight, 0);
-AudioConnection          patchCord5(lineIn, 1, biquadLowpassRight, 0);
-AudioConnection          patchCord6(lineIn, 1, biquadHighpassRight, 0);
-AudioConnection          patchCord7(lineIn, 1, biquadBandpassRight, 0);
-AudioConnection          patchCord8(biquadLowpassRight, 0, mixerRight, 0);
-AudioConnection          patchCord9(biquadLowpassLeft, 0, mixerLeft, 0);
-AudioConnection          patchCord10(biquadHighpassLeft, 0, mixerLeft, 1);
-AudioConnection          patchCord11(biquadHighpassRight, 0, mixerRight, 1);
-AudioConnection          patchCord12(biquadBandpassLeft, 0, mixerLeft, 2);
-AudioConnection          patchCord13(biquadBandpassRight, 0, mixerRight, 2);
-AudioConnection          patchCord14(mixerRight, 0, lineOut, 1);
-AudioConnection          patchCord15(mixerLeft, 0, lineOut, 0);
-AudioControlSGTL5000     audioShield;    //xy=540,446
+AudioInputUSB            usbIn;          //xy=120.19999694824219,129.1999969482422
+AudioAnalyzePeak         peak_R;         //xy=326.20001220703125,240.1999969482422
+AudioAnalyzePeak         peak_L;         //xy=328.20001220703125,203.1999969482422
+AudioMixer4              mixer1;         //xy=372.20001220703125,139.1999969482422
+AudioFilterBiquad        biquad1;        //xy=538.2000122070312,123.19999694824219
+AudioFilterBiquad        biquad2;        //xy=538.2000122070312,163.1999969482422
+AudioOutputI2S           audioOut;       //xy=698.2000122070312,143.1999969482422
+AudioConnection          patchCord1(usbIn, 0, mixer1, 0);
+AudioConnection          patchCord2(usbIn, 0, peak_L, 0);
+AudioConnection          patchCord3(usbIn, 1, mixer1, 1);
+AudioConnection          patchCord4(usbIn, 1, peak_R, 0);
+AudioConnection          patchCord5(mixer1, biquad1);
+AudioConnection          patchCord6(mixer1, biquad2);
+AudioConnection          patchCord7(biquad1, 0, audioOut, 0);
+AudioConnection          patchCord8(biquad2, 0, audioOut, 1);
+AudioControlSGTL5000     audioShield;    //xy=147.1999969482422,276.20001220703125
 // GUItool: end automatically generated code
 
-const int audioShieldInput = AUDIO_INPUT_LINEIN;
+
+#define NUM_LEDS 24
+#define DATA_PIN 33
+
+CRGB leds[NUM_LEDS];
+
+void blueColourGradient(CRGB* arr, uint8_t hue = 171, uint8_t sat = 255 , uint8_t val = 255) {
+  //creates a blue colour gradient
+  for (int led = 0; led < NUM_LEDS; led++) {
+    arr[led].setHSV( hue, sat, val);
+    arr[led].fadeToBlackBy(12 * led);
+  }
+}
+
+void rotateNeopixel(CRGB* arr) {
+  //move all elements in array to the right
+  CRGB tmp[NUM_LEDS];
+  int next;
+  for (int led = 0; led < NUM_LEDS; led++) {
+    next = (led + 1) % (NUM_LEDS);
+    tmp[led] = arr[next];
+  }
+
+  for (int led = 0; led < NUM_LEDS; led++) {
+    arr[led] = tmp[led];
+  }
+}
+
+void rotationAnimation(CRGB* arr, int T) {
+  blueColourGradient(leds);
+  for (int led = 0; led < NUM_LEDS; led++) {
+    rotateNeopixel(arr);
+    FastLED.show();
+    delay(T);
+    }
+}
+
+void showPeaks(CRGB* arr, float peakLeft = 0, float peakRight = 0) {
+  //some array magic to visualize the stereo peaks on the neopixel ring
+  int pLeft = int(peakLeft * (NUM_LEDS / 2));
+  int pRight = int(peakRight * (NUM_LEDS / 2));
+
+  //all leds are visible with reduced brightness
+  for (int led = 0; led < NUM_LEDS / 2; led++) {
+    arr[led].setHSV( 99 - (108 / (NUM_LEDS / 2)) * led, 220, 75);
+  }
+  for (int led = 0; led < NUM_LEDS / 2; led++) {
+    arr[NUM_LEDS / 2 + led].setHSV(108 / (NUM_LEDS / 2)*led, 220, 75);
+  }
+
+  //increase brightness according to the peak
+  for (int led = 0; led < pLeft; led++) {
+    arr[led].maximizeBrightness();
+    arr[led].fadeToBlackBy( 56 );
+  }
+  for (int led = 0; led < pRight; led++) {
+    arr[NUM_LEDS - 1 - led].maximizeBrightness();
+    arr[NUM_LEDS - 1 - led].fadeToBlackBy( 56 );
+  }
+}
 
 void setup() {
-  //allocate memory (https://www.pjrc.com/teensy/td_libs_AudioConnection.html)
+  //audio
   AudioMemory(12);
-  
-  //enable the audioShield (https://www.pjrc.com/store/teensy3_audio.html)
   audioShield.enable();
-  
-  //select the audioShield input
-  audioShield.inputSelect(audioShieldInput);
-  
-  //set the headphone volume(0.8 corresponds to the maximum undistorted output)
-  audioShield.volume(0.6);
+  audioShield.volume(0);
 
-  /*
-  https://www.pjrc.com/teensy/gui/index.html?info=AudioFilterBiquad:
-  setBandpass(stage, frequency, Q);
-	Configure one stage of the filter (0 to 3) with
-	band pass response. The filter has unity gain
-	at the specified frequency. Q controls the width
-	of frequencies allowed to pass. 
-  */
-  biquadLowpassRight.setLowpass(0, 400, 0.700);
-  biquadLowpassLeft.setLowpass(0, 400, 0.700);
-  biquadHighpassRight.setHighpass(0, 5000, 0.700);
-  biquadHighpassLeft.setHighpass(0, 5000, 0.700);
-  biquadBandpassRight.setBandpass(0, 400, 0.100);
-  biquadBandpassLeft.setBandpass(0, 400, 0.100);
+  //lowpass and highpass with two biquad filters
+  biquad1.setLowpass(0, 2000, 0.5);   //left signal : lowpass    (chinch: black)
+  biquad2.setHighpass(0, 2000, 0.5);  //right signal : highpass  (chinch: red, white)
+
+  //neopixel setup
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  //will show the peak animation if both peaks are available. If this isn't the case it will show the 'waiting animation'
+  if (peak_L.available() && peak_R.available()) {
+    showPeaks(leds, peak_L.read(), peak_R.read());
+    FastLED.show();
+    delay(80);
+  }
+  else {
+    rotationAnimation(leds, 150);
+  }
+
+  //use usbIn.volume() to get the USB volume
+  //recommended use: audioShield.volume(usbIn.volume()*0.7); //multiply with 0.7 to avoid distortion
+  audioShield.volume(0.4); //USB volume set to 0.4 to minimize tda7376B thermanl output
 }
